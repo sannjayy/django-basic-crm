a9425d34610c9af3ab424c0ffefcf7d86bef88d0
Sanjay Sikdar <znasofficial@gmail.com>
2020-05-30 15:15:53 +0530

    URL CHANGE

--- crm/settings.py
+++ crm/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'sc9p--b(+i^_(r6*k38hpemgj$_klo=w%+1+1*(zr=9(a7__*_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['crmlive-cli-django.herokuapp.com',  '127.0.0.1']
+ALLOWED_HOSTS = ['znas-crm-test.herokuapp.com',  '127.0.0.1']
 
 
 # Application definition
